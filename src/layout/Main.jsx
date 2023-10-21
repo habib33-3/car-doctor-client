a36248950ea4db46b0ce114082bff7fb59119265
Habibur Rahman <habib.swi16@gmail.com>
2023-10-21 23:34:59 +0600

    created navbar

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "../shared/Navbar/Navbar";
 import Footer from "../shared/Footer/Footer";
 
 const Main = () => {
   return (
-    <div>
+    <div className="max-w-7xl mx-auto">
+      <Navbar />
       <Outlet />
       <Footer />
     </div>
